29a6d35fa6e4a109b01b9b6c4ea4406c9db7cd98
ChoiHyoSik <huchoi24@naver.com>
2021-11-28 04:01:09 +0900

    appBar 및 CollapsingToolBar 초기 세팅

--- app/src/main/java/com/hyosik/android/collapsingtoolbarlayoutex/MainActivity.kt
+++ app/src/main/java/com/hyosik/android/collapsingtoolbarlayoutex/MainActivity.kt
@@ -1,11 +1,34 @@
 package com.hyosik.android.collapsingtoolbarlayoutex
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.WindowInsets
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.hyosik.android.collapsingtoolbarlayoutex.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private var binding : ActivityMainBinding ?= null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val mainActivityBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(mainActivityBinding.root)
+        binding = mainActivityBinding
+        initViewSettings(mainActivityBinding)
+
+    }
+
+    private fun initViewSettings(mainActivityBinding: ActivityMainBinding) {
+
+        window.statusBarColor = Color.TRANSPARENT
+
+        ViewCompat.setOnApplyWindowInsetsListener(mainActivityBinding.coordinator) { v : View, insets : WindowInsetsCompat ->
+            insets.consumeSystemWindowInsets()
+        }
+
     }
 }
